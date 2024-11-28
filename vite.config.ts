3069cb04bf1887bac7ff4ef130399e9e0a8f910a
Johanna <hl105@wellesley.edu>
2024-11-27 23:42:18 -0500

    alpha two

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [vue()],
   base: "/",
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
       "@": join(__dirname, "client"),
